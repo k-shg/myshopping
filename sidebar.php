40b3da86deb4429820cf157f24209ba7c8195c45
九嶋 将吾 <s6h1o3go@gmail.com>
2019-04-21 20:34:38 +0900

    サイドバーをボタンにしてリッチにした

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,14 @@
-<section id="sidebar">
-    <a href="registProduct.php">商品を出品する</a>
-    <a href="profEdit.php">プロフィール編集</a>
-    <a href="board.php">掲示板一覧</a>
-    <a href="withdraw.php">退会</a>
+<section id="sidebar" class="sidebar-container">
+    <button class="sidebar sidebar__btn" type="button" name="button">
+        <a class="sidebar sidebar__link" href="registProduct.php">商品を出品する</a>
+    </button>
+    <button class="sidebar sidebar__btn" type="button" name="button">
+        <a class="sidebar sidebar__link" href="profEdit.php">プロフィール編集</a>
+    </button>
+    <button class="sidebar sidebar__btn" type="button" name="button">
+        <a class="sidebar sidebar__link" href="board.php">掲示板一覧</a>
+    </button>
+    <button class="sidebar sidebar__btn" type="button" name="button">
+        <a class="sidebar sidebar__link" href="withdraw.php">退会</a>
+    </button>
 </section>
