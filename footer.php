12fc038bd569d3372253d576e7eb006f786644bf
九嶋 将吾 <s6h1o3go@gmail.com>
2019-04-24 01:38:15 +0900

    footerを最下部に固定

--- footer.php
+++ footer.php
@@ -59,6 +59,14 @@ $(function(){
         //ファイルを読み込む
         Reader.readAsDataURL(file);
     });
+
+
+    // フッターを最下部に固定
+    var $ftr = $('#footer');
+    if( window.innerHeight > $ftr.offset().top + $ftr.outerHeight() ){
+      $ftr.attr({'style': 'position:fixed; top:' + (window.innerHeight - $ftr.outerHeight()) +'px;' });
+    }
+
 });
 
 </script>
