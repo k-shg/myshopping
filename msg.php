ada5c688a9353a066ca530273a87845f5a670f3c
九嶋 将吾 <s6h1o3go@gmail.com>
2019-04-23 19:10:09 +0900

    掲示板のレイアウトを変更

--- msg.php
+++ msg.php
@@ -58,90 +58,212 @@ $title = '連絡掲示板';
 require('head.php') ?>
     <body>
         <style media="screen">
-        .area-send-msg .btn-send {
-            width: 150px;
-            float: right;
-            margin-top: 0;
-            margin: 15px 0;
-            padding: 15px 30px;
-            border: none;
-            background: #333;
-            color: white;
-            font-size: 14px;
+
+        .board-container {
+            /* min-height: 600px; */
+            /* background: red; */
         }
 
-        .area-board .msg-cnt {
-            width: 80%;
-            height: 40px;
-            background: yellow;
-            margin-bottom: 30px;
-        }
-        .area-board .msg-cnt.msg-right{
-            float: right;
-        }
-        .area-board .msg-cnt.msg-left{
-            float: left;
-        }
-        .avater img {
-            width: 40px;
-            height: 40px;
-        }
-        .area-board .msg-cnt.msg-right .avater{
-            float: right;
-        }
-        .area-board .msg-cnt.msg-left .avater{
-            float: left;
-        }
-        .area-board .msg-cnt.msg-left .inr-txt{
-            background: #f6e2df;
-            padding-left: 10px;
-        }
-        .area-board .msg-cnt.msg-right .inr-txt{
-            background: #d2eaf0;
-            padding-right: 10px;
-        }
-        .inr-txt {
-            padding: 10px;
-        }
+        /*---------------------------------------
+         	取引情報
+        --------------------------------------*/
+
+
         .msg-info .title {
             text-align: center;
             color: black;
         }
-        .avater-img img {
+
+        .msg-info {
+            padding: 15px;
+            background: #f6f5f4;
+            display: flex;
+            justify-content: space-around;
+            margin-bottom: 20px;
+            border: 3px solid #bababa;
+        }
+
+        .partner-info {
+            margin-left: 15px;
+            display: flex;
+            flex-direction: row;
+        }
+        .partner-info__img {
+            width: 100px;
+            height: 100px;
+            margin-left: 50px;
+            object-fit: cover;
+        }
+        .partner-info__profile div  {
+            margin-bottom: 20px;
+        }
+
+
+        .product-info {
+            display: flex;
+            flex-direction: row;
+        }
+
+        .product-info__main{
+        }
+        .product-info__sub{
+            margin-left: 50px;
+        }
+        .product-info__sub div {
+            margin-bottom: 20px;
+        }
+        .product-info__img {
+            width: 100px;
+            object-fit: cover;
+            height: 100px;
+        }
+
+
+
+
+        /*---------------------------------------
+         	メッセージやりとり
+        --------------------------------------*/
+
+        .area-board {
+        	min-height: 500px;
+        	background: #ffffff;
+        	width: 80%;
+        	margin: 0 auto;
+            overflow: hidden;
+        }
+        .area-board__container {
+            width: 100%;
+            overflow: hidden;
+            margin-bottom: 5px;
+        }
+
+        .area-board__msg {
+            width: 100%;
+            margin-bottom: 30px;
+            overflow: hidden;
+        }
+        .area-board__msg--right{
+            float: right;
+        }
+        .area-board__msg--left{
+            float: left;
+        }
+
+        .area-board__avater--right{
+            float: right;
+            margin-left: 10px;
+        }
+        .area-board__avater--left{
+            float: left;
+            margin-right: 10px;
+        }
+        .area-board__img {
+            width: 50px;
+            height: 50px;
+            border-radius: 20px;
+            object-fit: cover;
+        }
+
+        .area-board__inr-txt {
+            padding: 15px;
+            width: 40%;
+            border-radius: 10px;
+            position: relative;
+        }
+
+        .area-board__inr-txt--left{
+            background: #f6e2df;
+            padding-left: 10px;
+            float: left;
+            margin-right: 50px;
+        }
+        .area-board__inr-txt--right{
+            background: #d2eaf0;
+            padding-right: 10px;
+            float: right;
+        }
+
+        .triangle {
+            position: absolute;
+            top: 3px;
+            border-top: 20px solid transparent;
+            border-bottom: 20px solid transparent;
+        }
+        .triangle--left {
+            border-right: 20px solid #f6e2df;
+            left: -10px;
+        }
+
+        .triangle--right {
+            border-left: 20px solid #d2eaf0;
+            right: -10px;
+        }
+
+
+
+        .area-board__date {
+            font-size: 10px;
+            height: 100%;
+        }
+
+        .area-board__date--right {
+            text-align: right;
+        }
+        .area-board__date--left {
+            text-align: left;
+        }
+
+
+        .area-board__avater {
+            float: left;
+        }
+        .avater__img {
             width: 80px;
             height: 80px;
             border-radius: 40px;
             margin-left: 50px;
         }
-        .msg-info {
-            padding: 15px;
-            background: #f6f5f4;
+
+
+
+
+        /*---------------------------------------
+            メッセージ送信
+        --------------------------------------*/
+
+        .area-send-msg {
+        	min-height: 200px;
+        	float: left;
+        	width: 100%;
+        	margin-top: 20px;
+        }
+
+
+        .area-send-msg__text-container {
             overflow: hidden;
-            margin-bottom: 20px;
         }
-        .avater-img {
-            float: left;
+
+
+        .area-send-msg__textarea {
+            background: #f6f5f4;
+            border: 3px solid #bababa;
+            background: #f6f5f4;
+            height: 120px;
+            margin: 0 auto;
+            margin-bottom: 10px;
         }
-        .partner-info {
-            float: left;
-            margin-left: 15px;
-            width: 500px;
+
+        .area-send-msg__btm {
+            color: #fff;
+            background: #0e3056;
+            margin: 0 auto;
+            width: 20%;
         }
-        .product-info {
-            float: left;
-            width: 300px;
+        .area-send-msg__btm:hover {
+            background: #ff5722;
         }
-        .product-info .left{
-            float: left;
-        }
-        .product-info .right{
-            float: left;
-            width: 200px;
-        }
-        .product-info img {
-            width: 70px;
-            height: 70px;
-        }
+
         </style>
         <div class="flash-msg js-flash-msg">
             <?php if(!empty($message)) echo $message; ?>
@@ -151,25 +273,25 @@ require('head.php') ?>
                 <h1 class="site-title">連絡掲示板</h1>
                 <div class="board-container site-width">
                     <div class="msg-info">
-                        <div class="avater-img">
-                            <img src="<?php echo $partner_data['pic'] ?>" alt="" class="avater">
-                        </div>
                         <div class="partner-info">
-                            <?php
-                            echo $partner_data['name'].'<br>';
-                            echo $partner_data['age'].'<br>';
-                            echo $partner_data['email'].'<br>';
-                             ?>
+                            <div class="partner-info__profile">
+                                <div>名前：<?php  echo $partner_data['name'];?></div>
+                                <div>年齢： <?php echo $partner_data['age']; ?></div>
+                                <div>email：<?php echo $partner_data['email'];?></div>
+                            </div>
+
+                             <div class="partner-info__avater">
+                                 <img src="<?php echo $partner_data['pic'] ?>" alt="" class="partner-info__img">
+                             </div>
                         </div>
                         <div class="product-info">
-                            <div class="left">
-                                取引商品<br>
-                                <img src="<?php echo showImg($product_data['pic']) ?>" alt="">
+                            <div class="product-info__main">
+                                <img class="product-info__img" src="<?php echo showImg($product_data['pic']) ?>" alt="">
                             </div>
-                            <div class="right">
-                                「<?php echo $product_data['name'] ?>」<br>
-                                値段: <?php echo $product_data['price'] ?>円<br>
-                                購入日: <?php echo $order_date ?><br>
+                            <div class="product-info__sub">
+                                <div>「<?php echo $product_data['name'] ?>」</div>
+                                <div>値段： <?php echo $product_data['price'] ?>円</div>
+                                <div>購入日： <?php echo $order_date ?></div>
                             </div>
                         </div>
                     </div>
@@ -177,26 +299,33 @@ require('head.php') ?>
                         <div class="area-board">
                             <?php foreach ($order_msg_data as $key => $value): ?>
                                 <?php if($value['from_user'] == $my_user_id): ?>
-                                <div class="msg-cnt <?php echo 'msg-right' ?>">
-                                    <div class="avater">
-                                        <img src="<?php echo $my_data['pic']?>" alt="">
+                                <div class="area-board__msg area-board__msg--right">
+                                    <div class="area-board__container">
+                                        <div class="area-board__avater--right">
+                                            <img class="area-board__img" src="<?php echo $my_data['pic']?>" alt="">
+                                        </div>
+                                        <p class="area-board__inr-txt area-board__inr-txt--right">
+                                            <span class="triangle triangle--right"></span>
+                                            <?php echo $value['msg'] ?>
+                                        </p>
                                     </div>
-                                    <p class="inr-txt">
-                                        <?php echo $value['msg'] ?>
-                                    </p>
-                                    <div style="text-align: right;">
+                                    <div class="area-board__date area-board__date--right">
                                         <?php echo $value['create_date'] ?>
                                     </div>
                                 </div>
                                 <?php else:?>
-                                <div class="msg-cnt <?php echo 'msg-left'; ?>">
-                                    <div class="avater">
-                                        <img src="<?php echo $partner_data['pic'] ?>" alt="">
+                                <div class="area-board__msg area-board__msg--left">
+                                    <div class="area-board__container">
+                                        <div class="area-board__avater--left">
+                                            <img class="area-board__img" src="<?php echo $partner_data['pic'] ?>" alt="">
+                                        </div>
+                                        <p class="area-board__inr-txt area-board__inr-txt--left">
+                                            <span class="triangle triangle--left"></span>
+                                            <?php echo $value['msg'] ?>
+                                        </p>
                                     </div>
-                                    <p class="inr-txt">
-                                        <?php echo $value['msg'] ?>
-                                    </p>
-                                    <div style="text-align: left">
+
+                                    <div class="area-board__date area-board__date--left">
                                         <?php echo $value['create_date'] ?>
                                     </div>
                                 </div>
@@ -213,8 +342,13 @@ require('head.php') ?>
                     </div>
                     <div class="area-send-msg">
                         <form method="post">
-                            <textarea name="msg" rows="12" cols="80" style="background: white;"></textarea>
-                            <input type="submit" name="name" value="送信" class="btn btn-send">
+                            <div class="area-send-msg__text-container">
+                                <textarea class="area-send-msg__textarea js-get-count" name="msg" placeholder="送信メッセージを入力してください"></textarea>
+                                <div class="count-countainer" style="float:right;">
+                                    <span class="js-show-count">0</span>/255文字
+                                </div>
+                            </div>
+                            <input type="submit" name="name" value="送信" class="btn btn-mid area-send-msg__btm">
                         </form>
                     </div>
                 </div>
